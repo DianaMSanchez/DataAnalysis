6482c26b48ad0ecac64c050ae833059c40c5276b
gemagcloud <gema.gcloud@gmail.com>
2024-02-26 23:43:41 +0100

    test3

--- Proyecto DataAnalysis/app/dbconnection.py
+++ Proyecto DataAnalysis/app/dbconnection.py
@@ -11,7 +11,7 @@ def conectadb ():
 
     # Comprobar si la conexión fue exitosa
     if conn.is_connected():
-        print('Conexión exitosa a la base de datos')
+        #print('Conexión exitosa a la base de datos')
         cursor = conn.cursor()
         return cursor
 
